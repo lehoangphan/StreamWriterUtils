1a891b0ff8dd3ca7c62d726c7d3ea54719fc5943
lehoangphan <45199576+lehoangphan@users.noreply.github.com>
2020-12-22 13:37:42 +0700

    Add files via upload

---
+++ TextWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StreamWriterUtils
+{
+    public class TextWriter
+    {
+        public bool Write(string path, string content)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file =
+                                                   new System.IO.StreamWriter
+                                                   (path, false, System.Text.Encoding.UTF8))
+                {
+                    file.WriteLine(content);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
